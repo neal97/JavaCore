c49e0f75fa5a2492a8d75c01e506c13141e5f7a4
Neal Belfan <neal973@live.fr>
2025-03-18 19:14:48 +0100

    second commit

--- src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
+++ src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
@@ -3,7 +3,7 @@ package javacore.chapter02.condition.exercise;
 public class FrenchRevenueTaxCalculator {
     public static void main(String[] args){
 
-        double salary = 200000;
+        double salary = 20000;
         double impot ;
 
         double trancheMax1 = 0;
@@ -11,31 +11,33 @@ public class FrenchRevenueTaxCalculator {
         double trancheMax3 = 16063.2;
         double trancheMax4 = 38853.65;
 
-        if (salary > 11294 ){
 
-            impot = (salary - 11294) * 0.11 + trancheMax1 ;
-            System.out.println("avec un salaire de " + salary + " euros vous aurez " + impot + " d'euros a payer" );
-        }
-        else if (salary > 28797) {
-
-            impot = (salary - 28798) * 0.3 + trancheMax1 + trancheMax2 ;
-            System.out.println("avec un salaire de " + salary + " euros vous aurez " + impot + " d'euros a payer" );
-
-        }
-        else if (salary > 82341) {
-
-            impot = (salary - 82342) * 0.41 + trancheMax1 + trancheMax2 + trancheMax3;
-            System.out.println("avec un salaire de " + salary + " euros vous aurez " + impot + " d'euros a payer" );
-
-        }
-        else if (salary > 177106) {
+        if (salary > 177106) {
 
             impot = (salary - 177106) * 0.45 + trancheMax1 + trancheMax2 + trancheMax3 + trancheMax4;
             System.out.println("avec un salaire de " + salary + " euros vous aurez " + impot + " d'euros a payer" );
         }
-        else {
-            System.out.println("vous n'avez pas d'impot a payer");
+
+        if (salary <= 177106) {
+
+            impot = (salary - 82342) * 0.41 + trancheMax1 + trancheMax2 + trancheMax3;
+            System.out.println("avec un salaire de " + salary + " euros vous aurez " + impot + " d'euros a payer" );
         }
 
+        if (salary < 82342) {
+
+            impot = (salary - 28797) * 0.3 + trancheMax1 + trancheMax2;
+            System.out.println("avec un salaire de " + salary + " euros vous aurez " + impot + " d'euros a payer" );
+        }
+
+        if (salary < 28798) {
+
+            impot = (salary - 11294) * 0.11 + trancheMax1;
+            System.out.println("avec un salaire de " + salary + " euros vous aurez " + impot + " d'euros a payer" );
+        }
+
+        if (salary < 11295) {
+            System.out.println("vous n'avez pas d'impot a payer" );
+        }
     }
 }
