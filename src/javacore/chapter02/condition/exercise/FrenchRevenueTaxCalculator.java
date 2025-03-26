5f1b714b1a7e50ee25d70b2269834a6c61e15793
Neal Belfan <neal973@live.fr>
2025-03-26 18:31:21 +0100

    third commit

--- src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
+++ src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
@@ -3,8 +3,10 @@ package javacore.chapter02.condition.exercise;
 public class FrenchRevenueTaxCalculator {
     public static void main(String[] args){
 
-        double salary = 20000;
+        double salary = 40000;
         double impot ;
+        double salaryNet;
+        double impotNet;
 
         double trancheMax1 = 0;
         double trancheMax2 = 1925;
@@ -14,26 +16,34 @@ public class FrenchRevenueTaxCalculator {
 
         if (salary > 177106) {
 
-            impot = (salary - 177106) * 0.45 + trancheMax1 + trancheMax2 + trancheMax3 + trancheMax4;
-            System.out.println("avec un salaire de " + salary + " euros vous aurez " + impot + " d'euros a payer" );
+            impot = (salary - 177106) * 0.45;
+            salaryNet = salary - impot;
+            impotNet = salary - salaryNet;
+            System.out.println("avec un salaire de " + salary + " euros vous aurez " + impotNet + " d'euros a payer" );
         }
 
         if (salary <= 177106) {
 
-            impot = (salary - 82342) * 0.41 + trancheMax1 + trancheMax2 + trancheMax3;
-            System.out.println("avec un salaire de " + salary + " euros vous aurez " + impot + " d'euros a payer" );
+            impot = (salary - 82342) * 0.41;
+            salaryNet = salary - impot;
+            impotNet = salary - salaryNet;
+            System.out.println("avec un salaire de " + salary + " euros vous aurez " + impotNet + " d'euros a payer" );
         }
 
         if (salary < 82342) {
 
-            impot = (salary - 28797) * 0.3 + trancheMax1 + trancheMax2;
-            System.out.println("avec un salaire de " + salary + " euros vous aurez " + impot + " d'euros a payer" );
+            impot = (salary - 28797) * 0.3;
+            salaryNet = salary - impot;
+            impotNet = salary - salaryNet;
+            System.out.println("avec un salaire de " + salary + " euros vous aurez " + impotNet + " d'euros a payer" );
         }
 
         if (salary < 28798) {
 
-            impot = (salary - 11294) * 0.11 + trancheMax1;
-            System.out.println("avec un salaire de " + salary + " euros vous aurez " + impot + " d'euros a payer" );
+            impot = (salary - 11294) * 0.11 ;
+            salaryNet = salary - impot;
+            impotNet = salary - salaryNet;
+            System.out.println("avec un salaire de " + salary + " euros vous aurez " + impotNet + " d'euros a payer" );
         }
 
         if (salary < 11295) {
