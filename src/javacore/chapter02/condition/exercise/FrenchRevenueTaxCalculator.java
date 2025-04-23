0da08e5077cd842ddb334e5bbcc655f4b29e9537
Neal Belfan <neal973@live.fr>
2025-04-23 11:49:04 +0200

    4eme commit

--- src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
+++ src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
@@ -3,34 +3,39 @@ package javacore.chapter02.condition.exercise;
 public class FrenchRevenueTaxCalculator {
     public static void main(String[] args){
 
-        double salary = 20000;
+        double salary = 40000;
+        double salaryNet = 20000;
         double impot = 0 ;
 
-        if (salary > 177106){
+        if (salary > 177106) {
 
             impot += (salary - 177105) * 0.45;
+            salary -= (salary - 177105);
         }
 
         if (salary > 82342){
 
             impot += (salary - 82341) * 0.41;
+            salary -= (salary - 82341);
         }
 
         if (salary > 28798){
 
             impot += (salary - 28797) * 0.3;
+            salary -= (salary - 28797);
         }
 
         if (salary > 11295){
 
             impot += (salary - 11294) * 0.11;
+            salary -= (salary - 11294);
+        }
+        else {
+            System.out.println("avec un salaire de " + salaryNet + " euros vous aurez 0 euros d'impot a payer" );
         }
 
-        if (salary < 11295){
-            System.out.println("avec un salaire de " + salary + " euros vous aurez 0 euros d'impot a payer" );
-        }
-
-        System.out.println("avec un salaire de " + salary + " euros vous aurez " + impot + " d'euros a payer");
+        System.out.println("avec un salaire de " + salaryNet + " euros vous aurez " + impot + " d'euros a payer");
 
     }
 }
+
